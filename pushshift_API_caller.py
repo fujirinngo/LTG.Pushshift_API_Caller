22b0aa66ffabc4460a977ecce6ac899e66e0d692
fuji_rinngo <ssfujimo@ucdavis.edu>
2020-07-01 13:16:40 -0700

    added main file

---
+++ pushshift_API_caller.py
@@ -0,0 +1,22 @@
+import sys
+from src.input import Input
+from src.date_converter import convert2unix
+
+if __name__ == "__main__":
+    subreddit_list = ["UCDavis", "BROCKHAMPTON", "MachineLearning"] # should this be a parameter for a function instead?
+    start_datetime = convert2unix("06/01/2020 00:00:00")
+    end_datetime = convert2unix("06/08/2020 00:00:00")
+    for subreddit in subreddit_list:
+        input = Input(subreddit, start_datetime, end_datetime)
+        url_for_submissions = input.make_url_for_submissions()
+        JSONobject_for_submissions = input.getPushshiftData(url_for_submissions)
+        input.get_author_count_for_submissions(JSONobject_for_submissions)
+
+        url_for_comments = input.make_url_for_comments()
+        JSONobject_for_comments = input.getPushshiftData(url_for_comments)
+        input.get_author_count_for_comments(JSONobject_for_comments)
+
+        input.clean_dict()
+
+
+
